3efdddf85f8beccc00204269aa45cc166b57383f
Noel1827 <noel.vargas5@upr.edu>
2020-12-31 20:06:54 -0400

    TicTacToe :)

---
+++ Excercises/NumberTriangle.cpp
@@ -0,0 +1,31 @@
+// #include <iostream>
+
+// using namespace std;
+
+// int main()
+// {
+//     int range = 4;
+//     // cout << "Enter the range;";
+//     // cin >> range;
+
+//     for (int i = 1; i < range; i++){
+//         for( int j = 1; j <=range-i; j++){
+//             cout << " ";
+//         }
+//         for(int k = 1; k <=i; k++){
+//             cout << k;
+//         }
+
+//     }
+// }  too hard and unnecesary
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+
+
+    return 0;
+}
