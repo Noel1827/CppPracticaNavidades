fd0d6e2a25b9a893499a0b3b02c9c24384c6202a
Noel1827 <noel.vargas5@upr.edu>
2021-01-17 19:52:57 -0400

    more code

---
+++ C++Recap/BankAccounts.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+static int tries = 3;
+bool Access()
+{
+    int password;
+    cout << "Type the four digit password you have " << tries << " tries." << endl;
+    cin >> password;
+    if (tries > 1 )
+    {
+        if (password == 1234)
+            return true;
+        else
+        {
+            cout << "Try Again" << endl;
+            tries--;
+            return Access();
+        }
+    }else return false;
+}
+
+int main()
+{
+
+    while (true)
+    {
+        if (Access())
+        {
+            break;
+        }else break;
+    }
+    cout << "You are out of the loop";
+
+    return 0;
+}
--- C++Recap/CallByReference.cpp
+++ C++Recap/CallByReference.cpp
@@ -11,9 +11,11 @@ void swap(int* y, int* x){
 int main()
 {
     int x = 500;
-    int y = 100;
-    swap(&x, &y);
-    cout << "The value of x is: " << x << endl;
-    cout << "The value of y is: " << y << endl;
+    // int y = 100;
+    // swap(&x, &y);
+    // cout << "The value of x is: " << x << endl;
+    // cout << "The value of y is: " << y << endl;
+    cout << x++;
+    cout << x;
     return 0;
 }
